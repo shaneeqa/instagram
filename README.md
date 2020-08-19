dde4ab9019118effbdcb82f7935d73ffd87d4b41
aneeqahmadh@gmail.com <37417454+shaneeqa@users.noreply.github.com>
2020-08-19 23:50:16 +0530

    initial commit, worked until Profiles and Posts

---
+++ README.md
@@ -0,0 +1,2 @@
+# instagramClone
+ Clone of instagram
---
+++ app/Http/Controllers/ProfilesController.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\User;
+
+class ProfilesController extends Controller
+{
+    public function index(User $user)
+    {
+
+        return view('profiles.index', compact('user'));
+    }
+
+    public function edit(User $user)
+    {
+        return view('profiles.edit', compact('user'));
+    }
+
+    public function update(User $user)
+    {
+        $data = request()->validate([
+            'title' => 'required',
+            'description' => 'required',
+            'url' => 'url',
+            'image' => '',
+        ]);
+
+        auth()->user()->profile->update($data);
+
+        return redirect("profile/{$user->id}");
+    }
+}
