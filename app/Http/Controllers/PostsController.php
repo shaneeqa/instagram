fa3656697c300df8c588699f3c6df97732b8d6e6
aneeqahmadh@gmail.com <37417454+shaneeqa@users.noreply.github.com>
2020-09-13 05:05:37 +0530

    Added homepage with posts from the following profiles

--- app/Http/Controllers/PostsController.php
+++ app/Http/Controllers/PostsController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Post;
 use Illuminate\Http\Request;
 use Intervention\Image\Facades\Image;
 
@@ -13,6 +14,13 @@ class PostsController extends Controller
         $this->middleware('auth');
     }
 
+    public function index()
+    {
+        $users = auth()->user()->following->pluck('user_id');
+
+        $posts = Post::whereIn('user_id', $users)->latest()->get();
+        return view('posts.index', compact('posts'));
+    }
     public function create()
     {
         return view('posts.create');
